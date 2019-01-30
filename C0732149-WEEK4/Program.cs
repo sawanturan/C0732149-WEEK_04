e4d05af5c2a3a7b0aaeb9915b1a8edfc6711f28c
visualstudio <visualstudio@122-PC23>
2019-01-30 11:20:41 -0500

    commit

--- C0732149-WEEK4/Program.cs
+++ C0732149-WEEK4/Program.cs
@@ -21,7 +21,28 @@ namespace C0732149_WEEK4
         //What Kind Of Data Do We Need In a Node?
 
         public village nextVillage;
+        public village previousVillage;
         public string VillageName;
         public bool isAstrildeHere = false;
+    }
+    class Countryside
+    {
+        village Maple = new village();
+        village Toronto = new village();
+        village Ajax = new village();
+
+            public void launch()
+        {
+            Maple.VillageName = "Maple";
+            Maple.nextVillage = Toronto;
+            Maple.previousVillage = null;
+             Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Toronto.previousVillage = Maple;
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage = Toronto;
+
+        }
     }
 }
